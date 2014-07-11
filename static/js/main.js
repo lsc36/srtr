e49b7ddf33a77f40e86e8e2f82e3531bf8cc15a5
Shao-Chuan Lee <lsc36x@gmail.com>
2014-07-11 15:42:16 +0800

    async update

--- static/js/main.js
+++ static/js/main.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
 			return false;
 		}
 	});
+	window.setTimeout(updater.poll, 0);
 });
 
 var nextword = function(word, callback) {
@@ -20,4 +21,36 @@ var nextword = function(word, callback) {
 	$.getJSON('/next', $.param({w: word}), function(data) {
 		callback(data);
 	});
+};
+
+var updater = {
+	errorSleepTime: 500,
+	position: null,
+
+	poll: function() {
+		var args = {};
+		if (updater.position) args.pos = updater.position;
+		$.getJSON('/update', $.param(args), updater.onSuccess)
+			.fail(updater.onError);
+	},
+
+	onSuccess: function(response) {
+		updater.update(response);
+		updater.errorSleepTime = 500;
+		window.setTimeout(updater.poll, 0);
+	},
+
+	onError: function(response) {
+		updater.errorSleepTime *= 2;
+		console.log("Poll error; sleeping for", updater.errorSleepTime, "ms");
+		window.setTimeout(updater.poll, updater.errorSleepTime);
+	},
+
+	update: function(response) {
+		updater.position = response.position;
+		var last_word = response.last_word;
+		$('#last-word').html(last_word);
+		$('#position').html('#' + updater.position);
+		$('#next-word').attr('placeholder', last_word[last_word.length - 1] + '...')
+	}
 };
